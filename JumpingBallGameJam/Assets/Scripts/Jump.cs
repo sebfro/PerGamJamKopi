520ccffda17d6538d24684e0274e20e2ca5ac3c8
sebfro <sebastian17pepp@gmail.com>
2017-03-15 22:14:19 +0100

    no message

---
+++ JumpingBallGameJam/Assets/Scripts/Jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    public Rigidbody2D rb2dBall;
+    private bool canJump = false;
+
+    // Use this for initialization
+    void Start () {
+        rb2dBall = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(canJump)
+                rb2dBall.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D collisionInfo)
+    {
+        canJump = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+    }
+}
